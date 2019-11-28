2ad1939883082168c7d59c39e8695a4887a607f3
Jevon Edmund <jevonedmund@Jevons-MacBook-Pro.local>
2019-11-28 23:42:40 +0700

    halo ini jevon :) apakabr

---
+++ Team 2/blibli_shortestpath.py
@@ -0,0 +1,100 @@
+GRAPH = {\
+            'AngkeHubs':          {'CawangWare': 15.12710285651573, 'CakungWare': 16.112523957085767, 'CeperWare': 14.170499463156164, 'KarawaciWare':19.793578347833392, 'CawangHubs':13.8878581831389,'CakungHubs':15.931964554663976,'BekasiHubs': 26.807001590446013,'Bogor_CitereupHubs':40.47880269735003,'KarawaciHubs':20.153171353277646},\
+            'CawangHubs':         {'CawangWare': 2.5699488358185536, 'CakungWare': 11.614005303684982, 'CeperWare': 24.315299343277864, 'KarawaciWare':27.187701737246925, 'AngkeHubs': 13.8878581831389,'CakungHubs':9.883481746343453,'BekasiHubs':15.668893440427224,'Bogor_CitereupHubs':28.306468503461595,'KarawaciHubs':28.445578099340757},\
+            'CakungHubs':         {'CawangWare': 7.822543503195838, 'CakungWare': 1.9246960875557693, 'CeperWare': 29.554487235017497, 'KarawaciWare':34.02246238333466, 'AngkeHubs': 15.931964554663976,'CawangHubs':9.883481746343453,'BekasiHubs':11.620288134242616,'Bogor_CitereupHubs':35.7488543318176,'KarawaciHubs':34.87138784502206},\
+            'BekasiHubs':         {'CawangWare': 13.214615011237102, 'CakungWare': 12.558495268831212, 'CeperWare':39.42541989291127,    'KarawaciWare':42.8092016486174, 'AngkeHubs': 26.807001590446013,'CawangHubs':15.668893440427224,'CakungHubs':11.620288134242616,'Bogor_CitereupHubs':31.104343852041325,'KarawaciHubs':43.993438785920326},\
+            'Bogor_CitereupHubs': {'CawangWare': 28.893066703307728, 'CakungWare': 37.658657701923076, 'CeperWare':44.377878358247514, 'KarawaciWare':42.74136682726722,'AngkeHubs':40.47880269735003,'CawangHubs':28.306468503461595,'CakungHubs':35.7488543318176,'BekasiHubs':31.104343852041325,'KarawaciHubs':44.72108065506408},\
+            'KarawaciHubs':       {'CawangWare': 30.79958233429566, 'CakungWare': 35.53680915695285, 'CeperWare':6.439110908418687, 'KarawaciWare':2.0271423254364493,'AngkeHubs':20.153171353277646,'CawangHubs':28.445578099340757,'CakungHubs':34.87138784502206,'BekasiHubs':43.993438785920326,'Bogor_CitereupHubs':44.72108065506408},\
+            'CawangWare' :        {'Bogor_CitereupHubs':0},\
+            'CakungWare' :        {'Bogor_CitereupHubs':0},\
+            'CeperWare'  :        {'Bogor_CitereupHubs':0},\
+            'KarawaciWare':       {'Bogor_CitereupHubs':0},\
+            'Customer1_e711ce48_95d8_4f38_9fca_a186d18ba497':{'AngkeHubs' : 9.04858413907838, 'CawangHubs': 5.407946368785313, 'CakungHubs': 8.696427182956443, 'BekasiHubs':18.171168984169373, 'Bogor_CitereupHubs': 33.57997297629111,'KarawaciHubs': 26.346394960071084 },\
+            'Customer2_05ef3c47_ee8c_4cff_8dc9_bdeac09f0e52':{'AngkeHubs' : 37.01192287574016, 'CawangHubs': 26.478190631279727, 'CakungHubs': 21.181222672095476, 'BekasiHubs':10.809638649855891, 'Bogor_CitereupHubs': 36.90614108387435,'KarawaciHubs': 54.76829792980292 },\
+            'Customer3_d028d3ca_810b_4abf_9997_393dc20f742d':{'AngkeHubs' : 124.86224716672562, 'CawangHubs': 111.30711632322648, 'CakungHubs': 110.7743413574414, 'BekasiHubs':99.16797967835953, 'Bogor_CitereupHubs': 94.85071835399054,'KarawaciHubs': 137.12705779007428 },\
+            'Customer4_ecdbac87_aee1_4bd5_8e21_a6f5d14a76d1':{'AngkeHubs' : 3.767803664008067, 'CawangHubs': 10.187950918410566, 'CakungHubs': 13.65738448467535, 'BekasiHubs':23.856422100509786, 'Bogor_CitereupHubs': 36.83962597236334,'KarawaciHubs': 21.303280537942733 },\
+            'Customer5_82abb495_0391_4c95_834d_940fa292bb3c':{'AngkeHubs' : 3.6619857814155243, 'CawangHubs': 11.013136849752518, 'CakungHubs': 12.285387498065246, 'BekasiHubs':23.174440262165614, 'Bogor_CitereupHubs': 38.6032454449069,'KarawaciHubs': 23.259630832711856 },\
+            'Customer6_86fd004a_e6b0_4ef1_9d0d_65881d210d38':{'AngkeHubs' : 5.090873769364678, 'CawangHubs': 10.532880192828419, 'CakungHubs': 10.847441152237526, 'BekasiHubs':21.89500715394421, 'Bogor_CitereupHubs': 38.51272117944823,'KarawaciHubs': 24.77539710281218 },\
+            'Customer7_3b65dfaa_42cb_45c0_9b5e_73f2ab2f59e9':{'AngkeHubs' : 6.891064244586103, 'CawangHubs': 7.545874634482555, 'CakungHubs': 9.837443939326882, 'BekasiHubs':20.05516497406672, 'Bogor_CitereupHubs': 35.49755170240296,'KarawaciHubs': 25.042665504760404 },\
+            'Customer8_24d4e5ab_e313_42bb_bc6b_f1e43d92454a':{'AngkeHubs' : 20.140319100297862, 'CawangHubs': 6.252512224342258, 'CakungHubs': 12.35778718484193, 'BekasiHubs':12.558472127956554, 'Bogor_CitereupHubs': 23.478126023945766,'KarawaciHubs': 33.43129466792434 },\
+            'Customer9_098ed402_eaaa_401b_8779_9bc9d464832b':{'AngkeHubs' : 13.94904770869148, 'CawangHubs': 1.6010505236447254, 'CakungHubs': 8.295008771999019, 'BekasiHubs':14.502441787284711, 'Bogor_CitereupHubs': 29.277569343332452,'KarawaciHubs': 29.49894637322511 },\
+            'Customer10_37e14dfc_4473_4b96_b85e_86f929eebac2':{'AngkeHubs' : 0.9393940179584637, 'CawangHubs': 14.823003477443745, 'CakungHubs': 16.62521715700079, 'BekasiHubs':27.606466662464545, 'Bogor_CitereupHubs': 41.37330893261215,'KarawaciHubs': 19.908099735099555 },\
+            'Customer11_331828ed_3bb3_42f9_b0af_f0a639a1ffc6':{'AngkeHubs' : 11.730173471129516, 'CawangHubs': 6.930412827648435, 'CakungHubs': 4.6176319840304085, 'BekasiHubs':15.11215918877913, 'Bogor_CitereupHubs': 34.80715185661707,'KarawaciHubs': 30.266642600311627 },\
+            'Customer12_161a88ae_9021_4ce9_8a1c_3fcc38752a11':{'AngkeHubs' : 8.377840278948069, 'CawangHubs': 7.337926974497855, 'CakungHubs': 7.929186563711936, 'BekasiHubs':18.42983491950003, 'Bogor_CitereupHubs': 35.622522529882865,'KarawaciHubs': 26.96900903450349 },\
+            'Customer13_cf0c521c_db65_4b54_8f12_64a3d33d2113':{'AngkeHubs' : 8.65341768412857, 'CawangHubs': 5.672454988054326, 'CakungHubs': 9.051654820830779, 'BekasiHubs':18.59084256406969, 'Bogor_CitereupHubs': 33.76353894964711,'KarawaciHubs': 25.951757678240558 },\
+            'Customer14_fd03a70b_2b2f_4e8e_88c3_cf6d6d4a60a3':{'AngkeHubs' : 3.439568710919478, 'CawangHubs': 10.87160652636422, 'CakungHubs': 12.601455230409151, 'BekasiHubs':23.36871485922671, 'Bogor_CitereupHubs': 38.29705614599743,'KarawaciHubs': 22.79529035481839 },\
+            'Customer15_fefd46d6_d2d0_45e8_a054_131fbeda8e74':{'AngkeHubs' : 10.929545398032813, 'CawangHubs': 2.9587424263812725, 'CakungHubs': 9.886217015953616, 'BekasiHubs':17.688710412559775, 'Bogor_CitereupHubs': 30.801507051319664,'KarawaciHubs': 26.318106360310818 },\
+            'Customer16_75410b2d_1918_4a0f_b2b7_720ce8c4e92a':{'AngkeHubs' : 11.651561138902789, 'CawangHubs': 3.0441420537557673, 'CakungHubs': 11.728860327221584, 'BekasiHubs':18.66049438721988, 'Bogor_CitereupHubs': 29.330146314342215,'KarawaciHubs': 25.405430265019397 },\
+            'Customer17_deeb2897_3a4f_4471_80a3_719bd73580d9':{'AngkeHubs' : 22.810543062340706, 'CawangHubs': 10.356320067831756, 'CakungHubs': 18.87965041314591, 'BekasiHubs':18.77722064491946, 'Bogor_CitereupHubs': 18.01458019719879,'KarawaciHubs': 31.742617567010193 },\
+            'Customer18_cc745d50_a0b3_4995_adc4_42e09f812250':{'AngkeHubs' : 11.333614378176355, 'CawangHubs': 24.71627254140138, 'CakungHubs': 27.043744503795697, 'BekasiHubs':38.12600169103361, 'Bogor_CitereupHubs': 48.94999589829628,'KarawaciHubs': 14.518242816776079},\
+            'Customer19_de908962_149c_457a_b27b_5ce9a4e808f9':{'AngkeHubs' : 5.490385321403981, 'CawangHubs': 12.981687389009036, 'CakungHubs': 18.306265037640504, 'BekasiHubs':27.886072130516606, 'Bogor_CitereupHubs': 36.83517605219829,'KarawaciHubs': 16.565233099974687 },\
+            'Customer20_d4a2be90_2c5c_490d_baab_18a7d9e2e896':{'AngkeHubs' : 8.163413293778047, 'CawangHubs': 12.90071088112893, 'CakungHubs': 19.54647098033435, 'BekasiHubs':28.337774236773168, 'Bogor_CitereupHubs': 34.858972485990456,'KarawaciHubs': 15.665509725939085 },\
+            'Customer21_66a7c70b_d026_4583_a119_745afc43e60a':{'AngkeHubs' : 7.428115833703204, 'CawangHubs': 7.471608014306108, 'CakungHubs': 13.709306448635008, 'BekasiHubs':22.508402434494595, 'Bogor_CitereupHubs': 33.058115806223746,'KarawaciHubs': 21.569042538931523 },\
+            'Customer22_e6c4e01c_2331_42de_81ba_36b80ff3bb5f':{'AngkeHubs' : 9.394829026810784, 'CawangHubs': 9.829798368221306, 'CakungHubs': 6.708806222776868, 'BekasiHubs':18.13938239176517, 'Bogor_CitereupHubs': 38.07269631814725,'KarawaciHubs': 29.036391230027274 },\
+            'Customer23_a4d052db_22cd_4754_ac1b_6b13eb36c5cf':{'AngkeHubs' : 3.0509863863479785, 'CawangHubs': 13.343707161001051, 'CakungHubs': 13.565154190585828, 'BekasiHubs':24.822235451949236, 'Bogor_CitereupHubs': 40.96673719891947,'KarawaciHubs':  23.19192394482865 },\
+            'Customer24_e540732d_4292_4db1_bc6d_208a37ad20ab':{'AngkeHubs' : 136.21369475600062, 'CawangHubs': 122.89071011446603, 'CakungHubs': 121.63354699337974, 'BekasiHubs':110.01966827944739, 'Bogor_CitereupHubs': 107.90379505343678,'KarawaciHubs': 149.36580307268122 },\
+            'Customer25_1cebf438_11c4_4137_982a_7862da1bae90':{'AngkeHubs' : 14.807151426992421, 'CawangHubs': 11.96488310404814, 'CakungHubs': 21.19336756716972, 'BekasiHubs':27.24146678528969, 'Bogor_CitereupHubs': 27.859823231669182,'KarawaciHubs': 18.799705298897166 },\
+            'Customer26_8f54d942_0595_41ce_a94a_74f73bc43495':{'AngkeHubs' : 17.882359329948365, 'CawangHubs': 13.091660072350557, 'CakungHubs': 22.789662162451467, 'BekasiHubs':27.58198772882899, 'Bogor_CitereupHubs': 25.01053931591366,'KarawaciHubs': 20.649692314150244 },\
+            'Customer27_8c4e348c_f586_428c_bcc5_91140022b8dd':{'AngkeHubs' : 18.940960817735554, 'CawangHubs': 12.118683354172507, 'CakungHubs': 21.985759031938333, 'BekasiHubs':25.85852388649928, 'Bogor_CitereupHubs': 22.888829656969655,'KarawaciHubs': 23.095154595242605 },\
+            'Customer28_9f4bc659_bba0_425a_bf46_7abedef98a99':{'AngkeHubs' : 73.04135627049551, 'CawangHubs': 83.44374522107634, 'CakungHubs': 88.87441822362648, 'BekasiHubs':98.83864632905951, 'Bogor_CitereupHubs': 95.55559010240619,'KarawaciHubs': 55.05034735534109 },\
+            'Customer29_c7cf71e1_a673_4a1a_8ace_cfecc8b70a90':{'AngkeHubs' : 8.580882057276629, 'CawangHubs': 21.363989265021807, 'CakungHubs': 24.512350754937728, 'BekasiHubs':35.27665282900471, 'Bogor_CitereupHubs': 45.28996294497501,'KarawaciHubs': 13.514327376121924},\
+            'Customer30_377e1d8d_5893_4e15_a073_dd8428b34862':{'AngkeHubs' :8.695159763823767, 'CawangHubs': 21.59343262655018, 'CakungHubs': 24.619821098335525, 'BekasiHubs':35.430806113753924, 'Bogor_CitereupHubs': 45.61291986707593,'KarawaciHubs': 13.697204794224518 },\
+
+
+
+
+
+
+        }
+
+
+def ucs(source, destination):
+    """Cheapest path from source to destination using uniform cost search
+    :param source: Source Hub name
+    :param destination: Destination Warehouse name
+    :returns: Cost and path for cheapest traversal
+    """
+    from queue import PriorityQueue
+    priority_queue, visited = PriorityQueue(), {}
+    priority_queue.put((0, source, [source]))
+    visited[source] = 0
+    while not priority_queue.empty():
+        (cost, vertex, path) = priority_queue.get()
+        if vertex == destination:
+            return cost, path
+        for next_node in GRAPH[vertex].keys():
+            current_cost = cost + GRAPH[vertex][next_node]
+            if not next_node in visited or visited[next_node] >= current_cost:
+                visited[next_node] = current_cost
+                priority_queue.put((current_cost, next_node, path + [next_node]))
+
+def main():
+    """Main function"""
+    {\
+            'AngkeHubs':          {'CawangWare': 15.12710285651573, 'CakungWare': 16.112523957085767, 'CeperWare': 14.170499463156164, 'KarawaciWare':19.793578347833392, 'CawangHubs':13.8878581831389,'CakungHubs':15.931964554663976,'BekasiHubs': 26.807001590446013,'Bogor_CitereupHubs':40.47880269735003,'KarawaciHubs':20.153171353277646},\
+            'CawangHubs':         {'CawangWare': 2.5699488358185536, 'CakungWare': 11.614005303684982, 'CeperWare': 24.315299343277864, 'KarawaciWare':27.187701737246925, 'AngkeHubs': 13.8878581831389,'CakungHubs':9.883481746343453,'BekasiHubs':15.668893440427224,'Bogor_CitereupHubs':28.306468503461595,'KarawaciHubs':28.445578099340757},\
+            'CakungHubs':         {'CawangWare': 7.822543503195838, 'CakungWare': 1.9246960875557693, 'CeperWare': 29.554487235017497, 'KarawaciWare':34.02246238333466, 'AngkeHubs': 15.931964554663976,'CawangHubs':9.883481746343453,'BekasiHubs':11.620288134242616,'Bogor_CitereupHubs':35.7488543318176,'KarawaciHubs':34.87138784502206},\
+            'BekasiHubs':         {'CawangWare': 13.214615011237102, 'CakungWare': 12.558495268831212, 'CeperWare':39.42541989291127,    'KarawaciWare':42.8092016486174, 'AngkeHubs': 26.807001590446013,'CawangHubs':15.668893440427224,'CakungHubs':11.620288134242616,'Bogor_CitereupHubs':31.104343852041325,'KarawaciHubs':43.993438785920326},\
+            'Bogor_CitereupHubs': {'CawangWare': 28.893066703307728, 'CakungWare': 37.658657701923076, 'CeperWare':44.377878358247514, 'KarawaciWare':42.74136682726722,'AngkeHubs':40.47880269735003,'CawangHubs':28.306468503461595,'CakungHubs':35.7488543318176,'BekasiHubs':31.104343852041325,'KarawaciHubs':44.72108065506408},\
+            'KarawaciHubs':       {'CawangWare': 30.79958233429566, 'CakungWare': 35.53680915695285, 'CeperWare':6.439110908418687, 'KarawaciWare':2.0271423254364493,'AngkeHubs':20.153171353277646,'CawangHubs':28.445578099340757,'CakungHubs':34.87138784502206,'BekasiHubs':43.993438785920326,'Bogor_CitereupHubs':44.72108065506408},\
+            'CawangWare' :        {'Bogor_CitereupHubs':0},\
+            'CakungWare' :        {'Bogor_CitereupHubs':0},\
+            'CeperWare'  :        {'Bogor_CitereupHubs':0},\
+            'KarawaciWare':       {'Bogor_CitereupHubs':0},\
+    }
+    
+    print('ENTER SOURCE :', end=' ')
+    source = input().strip()
+    print('ENTER GOAL :', end=' ')
+    goal = input().strip()
+    if source not in GRAPH or goal not in GRAPH:
+        print('ERROR: PLACE DOES NOT EXIST.')
+
+    else:
+        cost, cheapest_path = ucs(source, goal)
+        print('PATH COST =', cost)
+        print(' -> '.join(city for city in cheapest_path))  
+    
+
+if __name__ == '__main__':
+    main()
